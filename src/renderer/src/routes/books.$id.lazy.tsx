ae0a92badd5e45d44d73caa7626bd779f18f5e97
matovu-farid <matovu90@gmail.com>
2024-04-28 23:19:25 +0300

    Refactor theme management in BookView
    Replaced the hardcoded theme system with a dynamic one that utilizes a centralized theme configuration, improving maintainability and scalability. Introduced the use of MUI's ThemeProvider to apply consistent theming across the UI. Removed redundant theme-related code and now fetching theme properties from a shared theme resource. This change not only streamlines theme application within the book reader but also facilitates easier theme customization and expansion in the future.

--- src/renderer/src/routes/books.$id.lazy.tsx
+++ src/renderer/src/routes/books.$id.lazy.tsx
@@ -12,43 +12,27 @@ import { Rendition } from 'epubjs'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import { usePopupState, bindTrigger, bindMenu } from 'material-ui-popup-state/hooks'
-import { Label } from '@mui/icons-material'
+import { AspectRatioRounded, Label } from '@mui/icons-material'
+import { ThemeType } from '@renderer/themes/common'
+import { themes } from '@renderer/themes/themes'
+import createIReactReaderTheme from '@renderer/themes/readerThemes'
+import { ThemeProvider, THEME_ID, createTheme } from '@mui/material/styles'
 
 export const Route = createLazyFileRoute('/books/$id')({
   component: () => <BookView />
 })
 
-type ITheme = 'light' | 'dark'
-
-const darkColor = 'rgb(48 48 50)'
-const darkTextColor = 'rgb(184 184 185)'
-const lightColor = 'rgb(246 240 226)'
-const lightTextColor = 'rgb(43 42 40)'
-
-function updateTheme(rendition: Rendition, theme: ITheme) {
-  const themes = rendition.themes
-
-  switch (theme) {
-    case 'dark': {
-      themes.override('color', darkTextColor)
-      themes.override('background', darkColor)
-      themes.override('font-size', '1.2em')
-
-      break
-    }
-    case 'light': {
-      themes.override('color', lightTextColor)
-      themes.override('background', lightColor)
-      themes.override('font-size', '1.2em')
-      break
-    }
-  }
+function updateTheme(rendition: Rendition, theme: ThemeType) {
+  const reditionThemes = rendition.themes
+  reditionThemes.override('color', themes[theme].color)
+  reditionThemes.override('background', themes[theme].background)
+  reditionThemes.override('font-size', '1.2em')
 }
 
 function BookView(): JSX.Element {
   const { id } = Route.useParams()
   const rendition = useRef<Rendition | undefined>(undefined)
-  const [theme, setTheme] = useState<ITheme>('dark')
+  const [theme, setTheme] = useState<ThemeType>(ThemeType.White)
   const popupState = usePopupState({ variant: 'popover', popupId: 'demoMenu' })
   useEffect(() => {
     if (rendition.current) {
@@ -68,6 +52,13 @@ function BookView(): JSX.Element {
       if (!book) throw new Error('Book not found')
 
       return book
+    }
+  })
+  const materialTheme = createTheme({
+    palette: {
+      primary: {
+        main: themes[theme].color
+      }
     }
   })
   const queryClient = useQueryClient()
@@ -96,126 +87,64 @@ function BookView(): JSX.Element {
 
   //   console.log(book)
   return (
-    <div className="relative">
-      <div className="absolute right-10 top-10 z-10">
-        <Link to="/">
-          <Button disabled={book.currentBookId === 0} variant="text" className="disabled:invisible">
-            Back
-          </Button>
-        </Link>
-        <IconButton {...bindTrigger(popupState)}>
-          <ListIcon color="primary" />
-        </IconButton>
-        <Menu {...bindMenu(popupState)}>
-          <RadioGroup
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue={theme}
-            name="radio-buttons-group"
-            sx={{ padding: '10px' }}
-            onChange={(e) => {
-              setTheme(e.target.value as ITheme)
-              popupState.close()
+    <ThemeProvider theme={{ [THEME_ID]: materialTheme }}>
+      <div className="relative">
+        <div className="absolute right-2 top-2 z-10">
+          <Link to="/">
+            <Button
+              disabled={book.currentBookId === 0}
+              variant="text"
+              className="disabled:invisible"
+            >
+              Back
+            </Button>
+          </Link>
+          <IconButton {...bindTrigger(popupState)}>
+            <ListIcon color="primary" />
+          </IconButton>
+          <Menu {...bindMenu(popupState)}>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue={theme}
+              name="radio-buttons-group"
+              sx={{ padding: '10px' }}
+              onChange={(e) => {
+                setTheme(e.target.value as ThemeType)
+                popupState.close()
+              }}
+            >
+              {(Object.keys(themes) as Array<keyof typeof themes>).map((theme) => (
+                <FormControlLabel key={theme} value={theme} control={<Radio />} label={theme} />
+              ))}
+            </RadioGroup>
+          </Menu>
+        </div>
+
+        <div style={{ height: '100vh' }}>
+          <ReactReader
+            loadingView={
+              <div className="w-full h-screen grid items-center">
+                <Loader />
+              </div>
+            }
+            url={book.epubUrl}
+            title={book.title}
+            location={book.currentBookId || 0}
+            locationChanged={(epubcfi: string) => {
+              updateBookId.mutate({ book, newId: epubcfi })
             }}
-          >
-            <FormControlLabel value="light" control={<Radio />} label="light" />
-            <FormControlLabel value="dark" control={<Radio />} label="dark" />
-          </RadioGroup>
-        </Menu>
+            swipeable={true}
+            readerStyles={createIReactReaderTheme(themes[theme].readerTheme)}
+            getRendition={(_rendition) => {
+              updateTheme(_rendition, theme)
+              rendition.current = _rendition
+            }}
+            epubInitOptions={{
+              openAs: 'epub'
+            }}
+          />
+        </div>
       </div>
-
-      <div style={{ height: '100vh' }}>
-        <ReactReader
-          loadingView={
-            <div className="w-full h-screen grid items-center">
-              <Loader />
-            </div>
-          }
-          url={book.epubUrl}
-          title={book.title}
-          location={book.currentBookId || 0}
-          locationChanged={(epubcfi: string) => {
-            updateBookId.mutate({ book, newId: epubcfi })
-          }}
-          swipeable={true}
-          readerStyles={theme === 'dark' ? darkReaderTheme : lightReaderTheme}
-          getRendition={(_rendition) => {
-            updateTheme(_rendition, theme)
-            rendition.current = _rendition
-          }}
-          epubInitOptions={{
-            openAs: 'epub'
-          }}
-        />
-      </div>
-    </div>
+    </ThemeProvider>
   )
-}
-const lightReaderTheme: IReactReaderStyle = {
-  ...ReactReaderStyle,
-  readerArea: {
-    ...ReactReaderStyle.readerArea,
-    background: lightColor
-  },
-  tocArea: {
-    ...ReactReaderStyle.tocArea,
-    background: lightColor
-  },
-  arrow: {
-    ...ReactReaderStyle.arrow,
-    // color: 'rgb(246 240 226)'
-    color: 'rgb(99 97 92)',
-    background: 'rgb(220 216 205)',
-    padding: '0px 5px',
-    borderRadius: '10px',
-    opacity: '0.3'
-  },
-  arrowHover: {
-    ...ReactReaderStyle.arrowHover,
-    ...ReactReaderStyle.arrow,
-    color: 'rgb(99 97 92)',
-    padding: '10px',
-    visibility: 'visible',
-    opacity: '1'
-    // background: 'rgb(220 216 205)'
-  }
-}
-
-const darkReaderTheme: IReactReaderStyle = {
-  ...ReactReaderStyle,
-  arrow: {
-    ...ReactReaderStyle.arrow,
-    color: darkTextColor
-  },
-  arrowHover: {
-    ...ReactReaderStyle.arrowHover,
-    color: darkTextColor
-  },
-  readerArea: {
-    ...ReactReaderStyle.readerArea,
-    backgroundColor: darkColor
-  },
-  titleArea: {
-    ...ReactReaderStyle.titleArea,
-    color: darkColor
-  },
-  tocArea: {
-    ...ReactReaderStyle.tocArea,
-    background: darkColor
-  },
-  tocButtonExpanded: {
-    ...ReactReaderStyle.tocButtonExpanded,
-    background: '#222'
-  },
-  tocButtonBar: {
-    ...ReactReaderStyle.tocButtonBar,
-    background: '#fff'
-  },
-  tocButton: {
-    ...ReactReaderStyle.tocButton,
-    color: 'white'
-  },
-  toc: {
-    ...ReactReaderStyle.toc,
-    color: 'white'
-  }
 }
