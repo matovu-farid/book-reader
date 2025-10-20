2acb1b98580e432d5c4f9ac595d7bf98f4a431a0
matovu-farid <matovu90@gmail.com>
2025-10-20 19:15:32 +0300

    Refactor EPUBJS components to TypeScript and enhance structure
    - Converted multiple JavaScript files to TypeScript, improving type safety and maintainability across the EPUBJS library.
    - Deleted outdated JavaScript files and replaced them with TypeScript counterparts, ensuring a consistent codebase.
    - Enhanced the organization of components such as `Annotations`, `Book`, `Contents`, and `Navigation`, streamlining their functionality and improving integration.
    - Updated import paths and adjusted method signatures to align with TypeScript standards, enhancing overall code clarity.
    
    These changes collectively improve the robustness and maintainability of the EPUBJS library, facilitating better development practices and future enhancements.

--- src/renderer/src/epubjs/types/index.d.ts
+++ src/renderer/src/epubjs/types/index.d.ts
@@ -2,19 +2,17 @@
 // Project: https://github.com/futurepress/epub.js#readme
 // Definitions by: Fred Chasen <https://github.com/fchasen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-import Epub from "./epub";
+import Epub from './epub'
 
-export as namespace ePub;
+export as namespace ePub
 
-export default Epub;
+export default Epub
 
-export { default as Book } from './book';
-export { default as EpubCFI } from './epubcfi';
-export { default as Rendition, Location } from './rendition';
-export { default as Contents } from './contents';
-export { default as Layout } from './layout';
-export { NavItem } from './navigation';
+export { default as Book } from './book'
+export { default as EpubCFI } from './epubcfi'
+export { default as Rendition, Location } from './rendition'
+export { default as Contents } from './contents'
+export { default as Layout } from './layout'
+export { NavItem } from './navigation'
 
-declare namespace ePub {
-
-}
+declare namespace ePub {}
